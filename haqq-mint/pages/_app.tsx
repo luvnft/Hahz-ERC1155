823b43af1007175066e78a049b13ce89ac335094
The Wizard of Hahz <info@luvnft.com>
2024-06-02 12:15:26 -0400

    updated

--- haqq-mint/pages/_app.tsx
+++ haqq-mint/pages/_app.tsx
@@ -1,29 +1,31 @@
 import type { AppProps } from "next/app";
-import { HaqqChainTestnet } from "@thirdweb-dev/chains";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Head from "next/head";
 import ThirdwebGuideFooter from "../components/GitHubLink";
 
+// Import the Base Mainnet configuration from Thirdweb SDK
+import { Base } from "@thirdweb-dev/chains";
+
 // This is the chain your dApp will work on.
-const activeChain = "HaqqChainTestnet ";
+const activeChain = BaseMainnet;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={HaqqChainTestnet}
-    clientId={process.env.TW_API_KEY}
+    <ThirdwebProvider 
+      activeChain={activeChain}
+      clientId={process.env.TW_API_KEY}
     >
-      
       <Head>
-        <title>HAQQ Network - ERC-1155 Edition Drop Minting Pages</title>
+        <title>Welcome To ATL5D</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="Using HAQQ Network and Thirdweb's Edition Drop contract to create customizable Edition Drop minting page"
+          content="Using Base Mainnet and Thirdweb's Edition Drop contract to create customizable Edition Drop minting page"
         />
         <meta
           name="keywords"
-          content="Haqq Network, Islamic Coin, ISLM, Thirdweb, thirdweb Edition drop, how to make thirdweb nft drop, how to make nft collection thirdweb"
+          content="Base Mainnet, Thirdweb, thirdweb Edition drop, how to make thirdweb nft drop, how to make nft collection thirdweb"
         />
       </Head>
       <Component {...pageProps} />
